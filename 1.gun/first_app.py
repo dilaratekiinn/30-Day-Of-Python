f57d43f212c4240dc47113e5eb2241d1d7c8aa87
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-06-01 15:56:22 +0300

    first commit

---
+++ 1.gun/first_app.py
@@ -0,0 +1,66 @@
+from operator import truediv
+from unicodedata import name
+
+
+print ("Merhaba") 
+
+
+maasAli = 5000
+maasAhmet =4000
+vergi= 0.27
+
+
+print (maasAli-(maasAli*0.27))
+print (maasAhmet-(maasAhmet*0.27))
+
+
+
+# Değişken tanımlama kuralları 
+# Rakam ile başlayamaz 
+
+number1 = 20 
+print(number1)
+
+
+number1 += 30
+print(number1)
+
+
+# Büyük küçük harf duyarlılığı 
+
+age =  20
+
+AGE = 30
+
+print (age)
+print (AGE)
+
+
+# Türkçe karakter kullanmayalım 
+
+yas =20 
+_age =20
+
+x = 1  # int 
+y = 2.3  # float 
+name = 'Ahmet' #string
+isStudent = True #bool
+
+
+ # x, y, name, isStudent = (1,2.3, 'Ahmet',True)
+
+
+a = '10'  #string
+b = '20'  #string
+print (a+b) #  => 1020 
+
+
+
+firstName = "Dilara"
+lastName = "Tekin"
+
+print (firstName + lastName)  # Dilara Tekin 
+
+
+
+
