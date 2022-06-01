6dea432198a1c944152833a410eff4a3d880d49b
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-06-01 16:55:01 +0300

    first commit

---
+++ 3.gun/type_conversion.py
@@ -0,0 +1,53 @@
+
+
+"""
+x =input ("1.Sayı:")
+y = input ("2.sayı:")
+
+print (type (x))
+print (type(y))
+
+toplam = int (x) + int (y)
+
+print (toplam)
+
+"""
+
+
+x = 5 #int
+y =2.5  #float 
+name ='dilara' #string
+isOnline = True #bool 
+
+
+# print (type(x))
+# print (type (y))
+# print (type(name))
+# print (type(isOnline))
+
+# Type conversion
+
+#int to float 
+x = float (x)
+print (x)
+print(type(x))
+
+#float to int 
+
+y = int (y)
+print (y)
+print (type(y))
+
+
+#bool to string 
+
+isOnline =str (isOnline)
+print(isOnline)
+print(type(isOnline))
+
+
+# bool to int 
+
+isOnline =int (isOnline)
+print(isOnline)
+print(type(isOnline))
