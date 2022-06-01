52a184bb2bdec76fbb89fa190709676ba7e8511a
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-06-01 16:20:16 +0300

    first commit

---
+++ 2.gun/variables-demo.py
@@ -0,0 +1,49 @@
+"""
+
+1- Bir müşterinin aşağıdaki bilgileri için değişken oluşturunuz.
+
+Müşteri adı 
+Müşteri soyadı
+Müşteri ad + soyad 
+Müşteri cinsiyet
+Müşteri tc kimlik 
+Müşteri doğum yılı 
+Müşteri adres bilgisi
+Müşteri yaşı 
+
+
+"""
+musteriAdi = 'Dilara'
+musteriSoyad = 'Tekin'
+musteriAdSoyad = musteriAdi + ' '  + musteriSoyad
+musteriCinsiyet = True # Erkek 
+musteriTcKimlik = '12345678912'
+musteriDogumYili = 2000
+musteriAdresi = 'İstanbul Sarıyer'
+musteriYas = 2022 - musteriDogumYili
+print (musteriYas)
+
+
+
+"""
+
+2- Aşağıdaki siparişlerin toplam bilgisini hesaplayınız.
+
+Sipariş 1 => 110 TL
+Sipariş 2 => 1100.5  TL
+Sipariş 3 => 356.95 TL
+
+"""
+   
+order1 = 110 
+order2 = 1100.5
+order3 = 356.95
+
+
+total = order1+order2+order3
+print ("Total : " , total)
+
+
+
+
+
