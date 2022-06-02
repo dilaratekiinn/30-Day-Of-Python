522229f53480c6fd08a62843ca5355dc41e75738
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-06-02 14:45:47 +0300

    first commit

---
+++ 5.gun/string.py
@@ -0,0 +1,26 @@
+ 
+# 
+
+# name = "dilara"
+# surName = "tekin"
+# age = 22
+
+# print ("My name is "+ name + '' + surName + 'and I am ' + 'years old')
+
+
+# 
+
+
+
+from unittest import result
+
+
+name =  "dilara" 
+surName = "tekin"
+
+# print ("My name is {} {}" .format(name,surName))
+
+result = 200 /700
+print ( " the result is { r:10.4} " .format(r=result))
+
+ 
