d2f2345674590aef90f2f4edf566fea0068ba3a5
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-06-02 17:10:20 +0300

    first commit

---
+++ 6.gun/tuple.py
@@ -0,0 +1,23 @@
+list = [1,2,3]
+
+tuple = (1,"iki",3)
+
+# print (type(list))
+# print(type(tuple))
+
+# print(list[2])
+# print(tuple[2])
+
+list = ["ali","veli"]
+tuple= ('damla', "ayşe","ayşe")
+
+
+
+list[0] = "ahmet"
+# tuple[0] = "dilara"
+
+print (tuple.count("ayşe"))
+
+
+print (list)
+print(tuple)
