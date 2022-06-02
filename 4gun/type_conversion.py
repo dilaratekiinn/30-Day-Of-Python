5c008d3fbb48d70c25ff61069ddd3153eede10a3
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-06-02 14:07:56 +0300

    first commit

---
+++ 4gun/type_conversion.py
@@ -0,0 +1,23 @@
+"""
+
+Daire alanı :  πr²
+Daire çevresi : 2πr
+
+
+* Yarı çapı verilen bir dairenin alan ve çevresini hesaplayınız.
+(r:3.14)
+
+"""
+
+pi = 3.14
+r = float (input ("yarı çap : ")) 
+
+alan = pi * (r ** 2 )
+cevre = 2 * pi * r 
+
+print ("alan", alan)
+print ("çevre",cevre )
+
+
+
+
