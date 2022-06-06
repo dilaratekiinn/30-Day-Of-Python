da5e5d8db55405879ab5d40472f9c074e6e3ca2b
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-06-06 18:32:04 +0300

    first commit

---
+++ egzersizler/app.py
@@ -0,0 +1,59 @@
+##Kullanıcıdan 3 tane sayı alın ve en büyük sayıyı ekrana yazdırın.
+
+
+# a =  int(input("a:"))
+
+# b = int(input("b:"))
+
+# c = int(input("c:"))
+
+# if (a >= b and a >= c):
+#     print("En büyük sayı:",a)
+# elif (b >= a and b >= c):
+#     print("En büyük sayı:",b)
+# elif (c >= a and c >= b):
+#     print("En büyük sayı:",c)
+    
+    
+    
+    
+    
+    
+    ## Kullanıcının girdiği vize1,vize2,final notlarına notlarına göre harf notunu hesaplayın.
+
+    # Vize1 toplam notun %30'una etki edecek.
+
+    # Vize2 toplam notun %30'una etki edecek.
+
+    # Final toplam notun %40'ına etki edecek.
+    
+    
+    
+#     vize1 = int(input("Vize1:"))
+# vize2 = int(input("Vize2:"))
+# final = int(input("Final:"))
+
+
+# genel_not =  vize1 * 3/10 + vize2 * 3/10 + final * 4/10
+
+# if (genel_not >= 90):
+#     print("AA")
+# elif (genel_not >= 85):
+#     print("BA")
+# elif (genel_not >= 80):
+#     print("BB")
+# elif (genel_not >= 75):
+#     print("CB")
+# elif (genel_not >= 70):
+#     print("CC")
+# elif (genel_not >= 65):
+#     print("DC")
+# elif (genel_not >= 60):
+#     print("DD")
+# elif (genel_not >= 55):
+#     print("FD")
+# else:
+#     print("FF")
+
+
+
