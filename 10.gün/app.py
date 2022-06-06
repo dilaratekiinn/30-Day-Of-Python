58796c21f7f063b47b7a40c41e7919a385e84cc9
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-06-06 18:33:18 +0300

    first commit

---
+++ 10.gün/app.py
@@ -0,0 +1,44 @@
+# toplam = 0
+# liste = [1,2,3,4,5,6]
+
+# for eleman in liste: 
+#     toplam= toplam + eleman 
+#     print ("Toplam {} Eleman: {}".format(toplam,eleman))
+#     print (toplam)
+    
+    
+
+# liste=  [1,2,3,4,34,54,63,78]
+# for eleman in liste:
+#    if eleman %2 == 0:
+#        print(eleman)
+
+
+
+# While fonksiyonu 
+
+# i = 0
+# while ( i<10):
+#     print ("İ'nin değeri",i)
+#     i+= 1  
+    
+    
+    
+ #  Range fonksiyonu
+ 
+# for i in range (1,100):
+#     print (i)
+    
+    
+#     for i in range (1,20):
+#         print("*"*i)
+        
+        
+        # Break ve Contiune
+        
+#          i = 0
+#        if (i == 5 )
+#           break 
+#       print ("i:",i)
+#           i+=1
+
