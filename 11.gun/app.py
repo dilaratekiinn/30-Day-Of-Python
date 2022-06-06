322f3ccd26cab323af5cde5d2e9cbc0d79fdbbd9
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-06-06 20:55:07 +0300

    first commit

---
+++ 11.gun/app.py
@@ -0,0 +1,71 @@
+
+
+# class Yazılımcı ():
+#     def __init__(self,isim,soyisim,numara,maas,diller):
+        
+#         self.isim=isim
+#         self.soyisim=soyisim
+#         self.numara=numara
+#         self.maas=maas
+#         self.diller=diller
+        
+#         def bilgilerigoster(self):
+#             print("""
+                  
+#                   Yazılımcı objesinin özellikleri
+                  
+#                 İsim: {}
+
+#                 Soyisim : {}
+                   
+#                 Numara : {}
+                   
+#                 Maaş :  {}
+                   
+#                 Bildiği Diller : {}
+                                
+        
+                  
+#                   """.format(self.isim,self.soyisim,self.numara,self.maas,self.diller))
+            
+#             yazılımcı = Yazılımcı("Dilara","Tekin", 12345,3000,[Python,JavaScript,SQL])
+   
+   
+   
+class calisan():
+    
+    def __init__(self,isim,maas,departman):
+        print("Çalışan sınıfının init fonksiyonu")
+        
+        
+        self.isim = isim
+        self.maas=maas
+        self.departman=departman
+        def  bilgilerigoster(self):
+            print("Çalışan sınıfının bilgileri...")
+            
+            print("İsim: {}\nMaas : {}\nDepartman {}\n".format(self.isim,self.maas,self.departman))
+            
+            def departman_degistir(self,yeni_departman):
+                self.departman=yeni_departman
+                
+                
+                class Yonetici(calisan):
+                    pass 
+                
+                yonetici=Yonetici("Dilara Tekin", 3000,"Bilişim")
+                
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
