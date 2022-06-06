bc4f9b242b017e25cf42830deb64002a672eaeac
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-06-06 11:08:53 +0300

    if-else

---
+++ 8.gun/app.py
@@ -0,0 +1,53 @@
+
+
+# if (koşul):
+
+     #if bloğu - koşul sağlanınca (True) çalışır. Bu hizadaki her işlem bu if bloğuna ait.
+     # if bloğu - girintiyle oluşur. 
+     # if bloğu eğer koşul sağlanırsa anlamını taşır. Eğer if kalıbındaki koşul sağlanırsa (True) if bloğu çalıştırır.
+     # eğer sağlanmazsa (False) if bloğu çalıştırılmaz. 
+
+
+# yas = int(input('Yaşınızı girin: '))
+
+# if yas < 18 :
+#     print ("Mekana giremezsiniz!")
+    
+# else: 
+#     print ("Mekana Hoş geldiniz!")
+    
+    # else bloğu 
+    #  else bloğu if koşulu sağlanmadığı zaman (False) çaloşan bloklardır. 
+    
+
+# #ornek
+
+# sayi=int(input("Sayı:"))
+
+# if sayi <0:
+#      print("Negatif Sayı")
+     
+# else: 
+#      print("Sıfır veya pozitif sayı")
+
+#     #ornek
+    
+# islem=input("İşlem giriniz:")
+
+# if islem == "1":
+#      print("İşlem 1 seçildi")
+# elif islem == "2":
+#      print ("İşlem 2 seçildi")
+# elif islem == "3":
+#      print("İşlem 3 seçildi")
+# else: 
+#      print ("Geçersiz İşlem!")
+     
+ 
+
+        
+
+             
+     
+     
+       
