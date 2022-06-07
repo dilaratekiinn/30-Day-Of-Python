3ac1738d44e6f0b5e40e98866eb1b458ea2e8ee4
Dilara Tekin <dilara-tekin2000@hotmail.com>
2022-06-07 13:29:17 +0300

    first commit

---
+++ 12.gun/app.py
@@ -0,0 +1,37 @@
+
+
+
+# from pickle import FALSE
+
+#   FİLTER FONKSİYONU 
+
+#  list(filter(lambda x: x % 2 == 0 , [1,2,3,4,5,6,7,8]))
+
+# Örnek
+
+# def asal_mi (x):
+#     i=2
+    
+#     if (x ==1):
+#         return False
+#     elif (x== 2):
+#         return True
+#     else:
+#         while(i<x):
+#             if (x% 1 ==0):
+#                 return False
+#             i+=1
+#             return True
+        
+#         list(filter(asal_mi,range(1,100)))
+        
+        
+        
+        #  ZİP FONKSİYONU
+           
+#      liste1 = [1,2,3,4,5]
+#      liste2 = [6,7,8,9,10,11]
+#      liste3 = ["Python", "Php","Java","JavaScript"]
+#      list(zip(liste1,liste2,liste3))
+     
+        
